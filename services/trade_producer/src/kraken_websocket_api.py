5873c71de79a5fe4cb2e737e66b1f96b0ea7ad2a
Devopdevs <owoeyebabatunde01@gmail.com>
2024-09-27 18:55:08 -0700

    Still implementing TP but websocket connection fail

--- services/trade_producer/src/kraken_websocket_api.py
+++ services/trade_producer/src/kraken_websocket_api.py
@@ -1,10 +1,13 @@
+import json
 from typing import List
 from websocket import create_connection
-
+from loguru import logger
 class KrakenWebsocketAPI:
     """
     Class for reading real-time trades from the Kraken Websocket API
     """
+    
+    URL = 'wss://ws.kraken.com/v2'
 
     def __init__(self, product_id: str):
         """
@@ -17,27 +20,64 @@ class KrakenWebsocketAPI:
 
         self.product_id = product_id
 
+
+        # establish connection to the Kraken websocket API
+        self._ws = create_connection(self.URL)
+        logger.debug('Connection established')
+
+        # subscribe to the trades for the given `product_id`
+        self._subscribe(product_id)
+
     def get_trades(self) -> List[dict]:
         """
         Returns the latest batch of trades from the Kraken Websocket API
         """
-        event = [{
-            "product_id": "Eth/USD",
-            "price": 1000,
-            "qty": 1,
-            "timestamp_ms": 1630000000000,
-        }]
+        message = self._ws.recv()
 
-        from time import sleep
-        sleep(1)
+        if 'heartbeat' in message:
+            # when I get a heartbeat, I return an empty list
+            return []
 
-        return event
+        # parse the message string as a dictionary
+        message = json.loads(message)
+
+        # extract trades from the message['data'] field
+        trades = []
+        for trade in message['data']:
+
+            breakpoint()
+
+        return trades
     
     
-def is_done(self) -> bool:
-    """
-    Returns True if the Kraken Websocket API is closed
-    """
-    False
+    def is_done(self) -> bool:
+        """
+        Returns True if the Kraken Websocket API is closed
+        """
+        False
+
+    def _subscribe(self, product_id: str):
+        """
+        Establish connection to the Kraken websocket API and subscribe to the trades for the given `product_id`.
+        """
+        logger.info(f'Subscribing to trades for {product_id}')
+        # let's subscribe to the trades for the given `product_id`
+        msg = {
+            'method': 'subscribe',
+            'params': {
+                'channel': 'trade',
+                'symbol': [product_id],
+                'snapshot': False,
+            },
+        }
+        self._ws.send(json.dumps(msg))
+        logger.info('Subscription worked!')
+
+        # For each product_id we dump
+        # the first 2 messages we got from the websocket, because they contain
+        # no trade data, just confirmation on their end that the subscription was successful
+        for product_id in [product_id]:
+            _ = self._ws.recv()
+            _ = self._ws.recv()
 
 
